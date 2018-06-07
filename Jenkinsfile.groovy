90a1dca1909cec3bccb44382a4d49bd13e92519f
Rob <brem_robert@hotmail.com>
2018-06-07 20:05:56 +0200

    initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+@Library('semantic_releasing') _
+
+podTemplate(label: 'mypod', containers: [
+        containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'curl', image: 'khinkali/jenkinstemplate:0.0.3', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'maven', image: 'maven:3.5.2-jdk-8', command: 'cat', ttyEnabled: true)
+],
+        volumes: [
+                hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
+        ]) {
+    node('mypod') {
+        properties([
+                buildDiscarder(
+                        logRotator(artifactDaysToKeepStr: '',
+                                artifactNumToKeepStr: '',
+                                daysToKeepStr: '',
+                                numToKeepStr: '30'
+                        )
+                ),
+                pipelineTriggers([])
+        ])
+
+        deployInfra('zipkin', 'kubeconfig.yml', false, false)
+    }
+}
